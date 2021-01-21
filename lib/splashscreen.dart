2d55496f4abdde271083e76b918c0b7b7ae3942c
prakhartiwari04 <writetoprakhartiwari@gmail.com>
2021-01-21 16:25:23 +0530

    Splash Screen added

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gaming/start.dart';
 
@@ -15,28 +16,25 @@ class _SplashScreenState extends State<SplashScreen> {
   // ignore: must_call_super
   void initState() {
     setState(() {
-      Timer(Duration(seconds: 3), () => {check()});
+      Timer(Duration(seconds: 5), () => {check()});
     });
   }
 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey[800],
         body: Center(
-          child: CircleAvatar(
-            backgroundColor: Colors.white,
-            radius: 150.0,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('assets/logo.png'),
-                ],
+            child: ClipOval(
+              child: Image.asset(
+                "assets/logo.png",
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
               ),
             ),
           ),
         ),
-      ),
     );
   }
 
