704305f525746edde8444868aa9d0777c02c1716
Chirag Agarwal <43610625+Chiragga24@users.noreply.github.com>
2021-01-21 15:56:18 +0530

    added splash screen

--- lib/search.dart
+++ lib/search.dart
@@ -67,4 +67,3 @@ class Search extends SearchDelegate {
   }
 }
   
-}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  // ignore: must_call_super
+  void initState() {
+    setState(() {
+      Timer(Duration(seconds: 3), () => {check()});
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 150.0,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/logo.png'),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void navigateUser() async {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MyHomePage(title: "Gaming App",),
+      ),
+    );
+  }
+
+  check() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MyHomePage(title: "Gaming App",),
+      ),
+    );
+  }
+}
